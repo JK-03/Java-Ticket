512448a5342fac5f4c09f2bd990aa386c6ce0dcc
jenniferbueso <jenniferbueso@jennifers-air>
2024-01-17 20:02:41 -0600

    Inicial

---
+++ src/javaticket/Main_JavaTicket.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package javaticket;
+
+/**
+ *
+ * @author jenniferbueso
+ */
+public class Main_JavaTicket {
+    
+    public static void main(String[] args) {
+        PantallaInicio pantallaInicio = new PantallaInicio();
+        pantallaInicio.setVisible(true);
+    }
+}
